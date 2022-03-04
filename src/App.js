16144a290a740e132cc42c85b33b99bda0f25879
Evander Owusu <owu.evander@gmail.com>
2022-03-04 00:09:17 +0000

    create TodoList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 // import "./styles.css";
 
 export default function App() {
+  const todos = [
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false },
+  ];
+
   return (
-    <div className="App">
+    <div>
       <h1>Todo List</h1>
+
+      <TodoList />
     </div>
   );
 }
+
+function TodoList() {}
