42d36b4b931e21857d73791d3372d088b95ccdb6
Evander Owusu <owu.evander@gmail.com>
2022-03-28 19:03:37 +0000

    add useState and handle new data that is added to list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import React, { useState } from "react";
 // import "./styles.css";
 
 export default function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, text: "Wash dishes", done: false },
     { id: 2, text: "Do laundry", done: false },
     { id: 3, text: "Take shower", done: false },
-  ];
+  ]);
 
   return (
     <div>
       <h1>Todo List</h1>
 
       <TodoList todos={todos} />
-      <AddTodo />
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
@@ -28,10 +29,22 @@ function TodoList({ todos }) {
   // console.log(props);
 }
 
-function AddTodo() {
+function AddTodo({ setTodos }) {
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false,
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    });
+  }
   return (
-    <form>
-      <input placeholder="Add todo" />
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add todo" />
       <button type="submit">Submit</button>
     </form>
   );
