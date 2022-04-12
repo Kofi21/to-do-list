632d206cd8a5e84fd11f341bc2f94d6e53dff43a
Evander Owusu <owu.evander@gmail.com>
2022-04-12 16:48:31 +0000

    complete todo list

--- src/App.js
+++ src/App.js
@@ -18,15 +18,63 @@ export default function App() {
   );
 }
 
-function TodoList({ todos }) {
+function TodoList({ todos, setTodos }) {
+  function handleToggleTodo(todo) {
+    const updatedTodos = todos.map((t) =>
+      t.id === todo.id
+        ? {
+            ...t,
+            done: !t.done,
+          }
+        : t
+    );
+    setTodos(updatedTodos);
+  }
+
+  if (!todos.length) {
+    return <p>No todos left!</p>;
+  }
+
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li
+          onClick={() => handleToggleTodo(todo)}
+          style={{ textDecoration: todo.done ? "line-through" : "" }}
+          key={todo.id}
+        >
+          {todo.text}
+          <DeleteTodo todo={todo} setTodos={setTodos} />
+        </li>
       ))}
     </ul>
   );
   // console.log(props);
+}
+
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
+  return (
+    <span
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer",
+      }}
+    >
+      x
+    </span>
+  );
 }
 
 function AddTodo({ setTodos }) {
